582d72d777427fbe02dad45aac7ce990d6c64f9f
tj-bu-706-11 <tj-bu-706-11@DESKTOP-F6JT9BB>
2023-06-19 11:49:51 +0900

    세번째 커밋

--- Along/src/quest/ArrayTest2.java
+++ Along/src/quest/ArrayTest2.java
@@ -4,11 +4,10 @@ public class ArrayTest2 {
 	public static void main(String[] args){
 		int[] number = new int[]{2,4,6,8,10};
 		int total = 0;
-		// test
+		
 		for(int i = 0; i<number.length; i++){
 			total += number[i];
 		}
-		
-		System.out.println("2부터 10까지 짝수의 합은 "+total+"입니다.");
+		System.out.println("2부터 10까지 짝수의 합은 "+total+"임둥.");
 	}
 }

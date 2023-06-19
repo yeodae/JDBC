d3ed81ec7b2ab9b4fff648c3efe589ddb699541b
tj-bu-706-11 <tj-bu-706-11@DESKTOP-F6JT9BB>
2023-06-19 11:44:46 +0900

    두번째 커밋연습(구구단테스트)

--- Along/src/quest/ArrayTest2.java
+++ Along/src/quest/ArrayTest2.java
@@ -4,7 +4,7 @@ public class ArrayTest2 {
 	public static void main(String[] args){
 		int[] number = new int[]{2,4,6,8,10};
 		int total = 0;
-		
+		// test
 		for(int i = 0; i<number.length; i++){
 			total += number[i];
 		}
---
+++ Along/src/quest/GitTest.java
@@ -0,0 +1,15 @@
+package quest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		
+		for(int i=2; i<10; i++){
+			for(int j=2; j<10; j++){
+				System.out.println(i+"x"+j+"="+i*j);
+			}
+		}
+
+	}
+
+}
